7aba815b9bd6b06eeb4d32f26cac9e8ade208cff
Ryan Kot <rjk0720@gmail.com>
2017-11-11 18:15:38 -0500

    Removed keys

--- keys.py
+++ keys.py
@@ -2,8 +2,8 @@
 # keys.py
 
 keys = dict(
-	consumer_key =          'LzqDeA04y8UlvYoIQFcGO4Gvu',
-	consumer_secret =       'wBxOq2VCLkvAHaklQ8bWz1LlOvlGry90Qs3p0DeMkzJaqCKrfr',
-	access_token =          '926670595416346624-GKZAkvqWVQJBK2fHBZ2ofQvxpIZLr2L',
-	access_token_secret =   'ekEyCxT1QXKSjEV2HNbGxSOfURcgIgQ0kzD36MuRI5F7F',
+	consumer_key =          '',
+	consumer_secret =       '',
+	access_token =          '',
+	access_token_secret =   '',
 )
